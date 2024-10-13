9cc45f581cda21c9406973c8152864f9149580dd
TetsuroKamada <tetsuro.kamada@cozmic.io>
2024-10-13 14:26:00 +0900

    feat: adjust follower reward and standby component

--- src/components/follower/Reward.tsx
+++ src/components/follower/Reward.tsx
@@ -1,47 +1,78 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
 import {css} from '@emotion/react';
-import {Box, Typography} from '@mui/material';
+import React from 'react';
 import OnigiriSenbei from '../../asset/rice_ball_senbei.png';
 
 const Reward = () => {
   return (
     <div css={containerStyle}>
-      <Box
-        sx={{
-          marginX: 6,
-          padding: 2,
-          backgroundColor: '#FFF0A5', // 薄黄色 (LemonChiffon)
-          borderRadius: '8px',
-          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-          height: '70%',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Typography variant="h5" sx={{marginTop: 2, marginBottom: 2, fontWeight: 'bold'}}>
-          ふうせんをあつめてくれてありがとう！
-        </Typography>
+      <h1 css={headingStyle}>ふうせんをあつめてくれて<br />ありがとう！</h1>
+      <div css={boxStyle}>
         <img
           src={OnigiriSenbei}
-          alt={`おにぎりせんべい`}
-          style={{maxHeight: '60%', borderRadius: '8px'}} // スタイルを追加
+          alt="おにぎりせんべい"
+          css={imageStyle}
         />
-        <Typography variant="body1" sx={{marginTop: 2, fontWeight: 'bold'}}>
-          おにぎり坊やからお礼をくれたよ。
+        <p css={paragraphStyle}>
+          おにぎり坊やからお礼をくれたよ。<br />
           ブースでおにぎりせんべいをうけとってね
-        </Typography>
-      </Box>
+        </p>
+      </div>
     </div>
   );
 };
 
+// コンテナのスタイル
 const containerStyle = css({
   display: 'flex',
   flexDirection: 'column',
-  flex: 1,
-  width: '100%',
   alignItems: 'center',
   justifyContent: 'center',
+  width: '100%',
+  padding: '16px',
+  boxSizing: 'border-box', // 余白も含めて調整
+});
+
+// 見出しのスタイル (白い文字で、ボックスの外に表示)
+const headingStyle = css({
+  color: '#FFFFFF', // 白い文字
+  fontWeight: 'bold',
+  fontSize: '1.8rem',
+  marginBottom: '16px',
+  textAlign: 'center',
+});
+
+// ボックスのスタイル（左右に余白を追加）
+const boxStyle = css({
+  padding: '16px',
+  backgroundColor: '#FFF0A5', // 薄黄色 (LemonChiffon)
+  borderRadius: '8px',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  width: '100%',
+  maxWidth: '500px',
+  textAlign: 'center', // テキストを中央揃え
+  marginLeft: '16px', // 左右の余白
+  marginRight: '16px',
+  boxSizing: 'border-box',
+  '@media (orientation: landscape)': {
+    maxWidth: '80vw', // 横向きのときは画面幅の80%に制限
+  },
+});
+
+// 画像のスタイル
+const imageStyle = css({
+  maxWidth: '100%',
+  maxHeight: '350px',
+  borderRadius: '8px',
+  height: 'auto', // 画像の比率を保つ
+  objectFit: 'contain', // はみ出しを防ぐための調整
+});
+
+// 段落のスタイル
+const paragraphStyle = css({
+  marginTop: '16px',
+  fontWeight: 'bold',
+  fontSize: '1.1rem',
 });
 
 export default Reward;
--- src/components/follower/Standby.tsx
+++ src/components/follower/Standby.tsx
@@ -1,75 +1,186 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
-import {Box, Typography} from '@mui/material';
-import Carousel from 'react-material-ui-carousel';
 import {css} from '@emotion/react';
+import React, {useState} from 'react';
+import {Carousel} from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // カルーセルのCSSをインポート
+import Bouya from '../../asset/01.png';
+import Christine from '../../asset/02.png';
+import Shionosuke from '../../asset/03.png';
+import Charlotte from '../../asset/04.png';
+import Logo2 from '../../asset/cozmic_horizontal_primary.png'; // ロゴ画像2をインポート
+import Logo1 from '../../asset/masuya_logo.png'; // ロゴ画像1をインポート
 
-const images = [
-  "http://www.masuya.co.jp/images/2024mentaimayo_banner.jpg",
-  "http://www.masuya.co.jp/images/20240310mijumaru_on.jpg",
-  "http://www.masuya.co.jp/images/main_onigiri_2023.jpg",
-  "http://www.masuya.co.jp/images/main_ginsyari_2023.jpg",
-  "http://www.masuya.co.jp/images/main_ginsyari_2.jpg",
-  "http://www.masuya.co.jp/images/main_pique.jpg"
+// 画像、キャラクター名、説明を含むオブジェクトの配列
+const characters = [
+  {
+    image: Bouya,
+    name: 'おにぎり坊や',
+    desc1: '三角形が大好き！',
+    desc2: '見た目とちがってまる～い性格。',
+    desc3: '将来の夢はスーパーヒーローになること！',
+  },
+  {
+    image: Christine,
+    name: 'クリスティーヌ',
+    desc1: 'パリッとした性格。',
+    desc2: 'チャームポイントは大きなリボン！',
+    desc3: '将来の夢はハリウッド女優になること♬',
+  },
+  {
+    image: Shionosuke,
+    name: 'しおのすけ',
+    desc1: 'あっさりした性格。',
+    desc2: '本当は優しいがときどき塩対応。',
+    desc3: 'ひとりごとをよく言う。',
+  },
+  {
+    image: Charlotte,
+    name: 'シャルロット',
+    desc1: 'おっとりした性格。',
+    desc2: 'しかし、いざとなるとものごとを',
+    desc3: 'ザクザク進められるしっかり者。',
+  },
 ];
 
 const Standby = () => {
+  const [currentSlide, setCurrentSlide] = useState(0); // 現在のスライドのインデックスを保持する
+
   return (
     <div css={containerStyle}>
-      <p
-        css={{
-          fontSize: '8vw',
-          fontWeight: 700,
-          textAlign: 'center',
-          color: 'white',
-        }}>
+      <p css={titleStyle}>
         イベント開始まで
         <br />
         しばらくお待ちください
       </p>
-      <Box
-        sx={{
-          marginX: 6,
-          padding: 2,
-          backgroundColor: '#FFF0A5', // 薄黄色 (LemonChiffon)
-          borderRadius: '8px',
-          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-          height: '70%',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Typography variant="h5" textAlign='center' >株式会社マスヤ</Typography>
-        <Carousel >
-          {images.map((src, index) => (
-            <img
-              key={index}
-              src={src}
-              alt={`マスヤ株式会社の画像 ${index + 1}`}
-              style={{width: '100%', borderRadius: '8px'}} // スタイルを追加
-            />
+      <div css={boxStyle}>
+        <Carousel
+          autoPlay
+          infiniteLoop
+          interval={3000}
+          showThumbs={false}
+          showStatus={false}
+          showArrows={true}
+          dynamicHeight={false}
+          css={{borderRadius: '8px'}}
+          onChange={(index) => setCurrentSlide(index)} // スライドが切り替わるときにインデックスを更新
+        >
+          {characters.map((char, index) => (
+            <div key={index}>
+              <img
+                src={char.image}
+                alt={`${char.name}の画像`}
+                css={imageStyle} // 画像のスタイルも調整
+              />
+            </div>
           ))}
         </Carousel>
-        <p css={{
-          fontSize: '3vw',
-          fontWeight: 500,
-          textAlign: 'left',
-        }}>
-          株式会社マスヤは、創業から長い歴史を持つ老舗企業で、品質にこだわった食品製造を行っています。<br />
-          詳細は公式サイトをご覧ください <a href="http://www.masuya.co.jp/" target="_blank" rel="noopener noreferrer">www.masuya.co.jp</a>
-        </p>
 
-      </Box>
+        {/* 現在のスライドのキャラクター名と説明を表示 */}
+        <div css={characterInfoStyle}>
+          <h4 css={characterNameStyle}>{characters[currentSlide].name}</h4>
+          <p css={characterDescriptionStyle}>
+            {characters[currentSlide].desc1}
+            <br />
+            {characters[currentSlide].desc2}
+            <br />
+            {characters[currentSlide].desc3}
+          </p>
+        </div>
+
+        <div css={logoContainerStyle}>
+          <a href="http://www.masuya.co.jp/" target="_blank" rel="noopener noreferrer">
+            <img src={Logo1} alt="株式会社マスヤ" css={logoStyle} />
+          </a>
+          <a href="https://www.cozmic.io/" target="_blank" rel="noopener noreferrer">
+            <img src={Logo2} alt="株式会社cozmic" css={logoStyle} />
+          </a>        </div>
+      </div>
     </div>
   );
 };
 
+// スタイル設定
 const containerStyle = css({
   display: 'flex',
   flexDirection: 'column',
-  flex: 1,
   width: '100%',
   alignItems: 'center',
   justifyContent: 'center',
+  padding: '2rem', // 画面に余白を持たせる
+});
+
+const titleStyle = css({
+  fontSize: '7vw', // スマホでもPCでも自動調整
+  fontWeight: 700,
+  textAlign: 'center',
+  color: 'white',
+  '@media (min-width: 768px)': { // タブレット・PC向けのフォントサイズ調整
+    fontSize: '3vw',
+  },
+});
+
+const boxStyle = css({
+  width: '100%', // モバイルでは画面幅いっぱい
+  padding: '1.6rem',
+  backgroundColor: '#FFF0A5', // 薄黄色 (LemonChiffon)
+  borderRadius: '8px',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  '@media (min-width: 1024px)': {
+    width: '70%', // PCでは幅を縮める
+  },
+});
+
+// キャラクター画像のスタイル
+const imageStyle = css({
+  width: '100%',
+  maxHeight: '400px',
+  objectFit: 'contain',
+  borderRadius: '8px',
+});
+
+// キャラクター情報のスタイル
+const characterInfoStyle = css({
+  marginTop: '0.8rem',
+  textAlign: 'left',
+});
+
+const characterNameStyle = css({
+  textAlign: 'center',
+  fontSize: '5vw',
+  fontWeight: 'bold',
+  color: '#333',
+  '@media (min-width: 768px)': {
+    fontSize: '2.5vw',
+  },
+});
+
+const characterDescriptionStyle = css({
+  fontSize: '4vw',
+  color: '#666',
+  '@media (min-width: 768px)': {
+    fontSize: '2vw',
+  },
+});
+
+// ロゴ表示部分のスタイル
+const logoContainerStyle = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexWrap: 'wrap', // モバイルでも表示が崩れないようにする
+  marginTop: '2rem',
+  gap: '1rem', // ロゴの間に隙間をつける
+});
+
+const logoStyle = css({
+  width: '8rem',
+  height: 'auto',
+  '@media (min-width: 768px)': {
+    width: '10rem', // PCでは少し大きく
+  },
 });
 
 export default Standby;
